b1d929675a9cfe99a45480b07716c4cbb06f1a44
maria1625 <166460177+maria1625@users.noreply.github.com>
2026-06-03 23:39:36 -0500

    synchronize false en produccion

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -15,7 +15,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
         autoLoadEntities: true,
-        synchronize: true,
+        synchronize: false,
         logging: false,
       }),
     }),
